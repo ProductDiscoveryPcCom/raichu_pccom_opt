98044df5d77ca309e104915efadf761acebbe0a4
ProductDiscoveryPcCom <product.discovery@pccomponentes.com>
2025-11-25 17:13:58 +0100

    Create __init__.py

---
+++ prompts/__init__.py
@@ -0,0 +1,13 @@
+"""
+Prompts package - PcComponentes Content Generator
+"""
+
+from . import new_content
+from . import rewrite
+
+__all__ = [
+    'new_content',
+    'rewrite'
+]
+
+__version__ = "4.1.1"
